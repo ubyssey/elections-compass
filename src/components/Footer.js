afa56075df593b369618521a1ee6177d4ae9b3cc
Peter Siemens <peterjsiemens@gmail.com>
2017-03-07 11:11:42 -0800

    Remove old footer code

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import '../styles/footer.css';
-
-export default function Footer(props) {
-  return (
-    <div className='c-ec-footer'>Produced by your friends at <em>The Ubyssey</em></div>
-  )
-}
